859e7ce58899b016ab521c359bb2a65f4ac89c33
stefanoscacchetti2004 <stefano.scacchetti01@universitadipavia.it>
2025-05-24 11:09:00 +0200

    Aggiornamento dipendenze

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Update pip and install dependencies
-echo "Updating pip and installing dependencies..."
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-# Start the application
-echo "Starting application..."
-uvicorn gucci_api:app --host 0.0.0.0 --port ${PORT:-8000} --reload
 uvicorn gucci_api:app --host 0.0.0.0 --port ${PORT:-8000}
